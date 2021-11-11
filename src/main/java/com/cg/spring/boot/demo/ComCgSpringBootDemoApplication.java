de2b862196b58b5c85471b28096eb3a9eeb87626
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-11 10:39:16 +0530

    C

--- src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoApplication.java
+++ src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoApplication.java
@@ -1,13 +1,14 @@
 package com.cg.spring.boot.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComCgSpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComCgSpringBootDemoApplication.class, args);
+
+		System.out.println("Vaman");
+
 	}
 
 }

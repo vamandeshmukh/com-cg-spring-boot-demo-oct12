c1c45ab31b1ab1e55084d7ad0ac78d152dea766a
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-11 10:29:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com-cg-spring-boot-demo-oct12
+ Spring boot demo project for CG trainees 12 Oct 2021 batch 3
---
+++ src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cg.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComCgSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComCgSpringBootDemoApplication.class, args);
+	}
+
+}

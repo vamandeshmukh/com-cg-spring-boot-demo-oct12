ef696d4be565ae64ca988ca6d4397e6f0cbf6a38
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-11 13:21:20 +0530

    Update Hello.java

--- src/main/java/com/cg/spring/boot/demo/Hello.java
+++ src/main/java/com/cg/spring/boot/demo/Hello.java
@@ -8,7 +8,6 @@ public class Hello {
 
 	// URL - http://localhost:8082/hello
 	// Endpoint, API - hello
-
 	@RequestMapping("/hello") // endpoint , API
 	public String hello() {
 		System.out.println("hello");
@@ -16,11 +15,20 @@ public class Hello {
 	}
 
 // http://localhost:8082/hi
-
 	@RequestMapping("/hi")
 	public String hi() {
 		System.out.println("hi");
 		return "Hi world!";
 	}
 
+	// http://localhost:8082/some-other-api
+	@RequestMapping("/some-other-api")
+	public String someOtherApi() {
+		System.out.println("someOtherApi");
+		return "<!DOCTYPE html>\r\n" + "<html>\r\n" + "\r\n" + "<head>\r\n" + "</head>\r\n" + "\r\n" + "<body>\r\n"
+				+ "    <h1 style='color: blue; font-family: Arial, Helvetica;'>Welcome to Capgemini</h1>\r\n"
+				+ "    <p style='color: darkblue; font-family: consolas, Monospace;'>You are most welcome to Capgemini.</p>\r\n"
+				+ "    <img src='https://www.capgemini.com/us-en/wp-content/uploads/sites/4/2018/08/cropped-cropped-capgemini_logo_color_rgb.png'>\r\n"
+				+ "</body>\r\n" + "\r\n" + "</html>";
+	}
 }

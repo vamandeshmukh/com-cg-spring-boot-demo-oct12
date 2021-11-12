60fcc737645bf5ccf25c878864547c42379ebe87
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-12 12:48:41 +0530

    C

--- src/main/java/com/cg/spring/boot/demo/service/EmployeeService.java
+++ src/main/java/com/cg/spring/boot/demo/service/EmployeeService.java
@@ -1,16 +1,12 @@
 package com.cg.spring.boot.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.cg.spring.boot.demo.model.Employee;
 import com.cg.spring.boot.demo.repository.EmployeeRepository;
-
-//@Component
 
 @Service
 public class EmployeeService {

3b26a800edb578117e1c93fcd587f0c4365b52b1
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-12 12:39:42 +0530

    Update EmployeeRepository.java

--- src/main/java/com/cg/spring/boot/demo/repository/EmployeeRepository.java
+++ src/main/java/com/cg/spring/boot/demo/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.cg.spring.boot.demo.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cg.spring.boot.demo.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }

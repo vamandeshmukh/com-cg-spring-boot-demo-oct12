b9848a80e3e825c7c3d7b298e84fed0b03103cb7
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-12 17:18:25 +0530

    Update EmployeeController.java

--- src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
+++ src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +42,8 @@ public class EmployeeController {
 		return empService.addEmployee(employee);
 	}
 
-	@PostMapping("/updateemp")
+	// http://localhost:8082/updateemp
+	@PutMapping("/updateemp")
 	public Employee updateEmp(@RequestBody Employee employee) {
 		System.out.println("Controller updateEmp");
 		return empService.updateEmployee(employee);

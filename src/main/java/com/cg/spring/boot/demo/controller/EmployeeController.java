15a47313c34238daddf612d32c236b29d56c4589
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-12 10:02:58 +0530

    Commit

--- src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
+++ src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
@@ -1,36 +1,60 @@
 package com.cg.spring.boot.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.spring.boot.demo.model.Employee;
-
+import com.cg.spring.boot.demo.service.EmployeeService;
+//@Component
 @RestController
 public class EmployeeController {
 
-	@GetMapping("/getemp")
-	public Employee getEmployee() {
-		System.out.println("getEmployee");
-		return new Employee(101, "Sonu", 10.5);
+	@Autowired
+	private EmployeeService empService;
+
+	// http://localhost:8082/getallemps
+	@GetMapping("/getallemps")
+	public List<Employee> getAllEmps() {
+		System.out.println("Controller getAllEmps");
+		return empService.getAllEmployees();
 	}
 
-	@GetMapping("/getallemp")
-	public List<Employee> getAllEmployees() {
-		System.out.println("getAllEmployees");
-		List<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(101, "Sonu", 10.5));
-		empList.add(new Employee(102, "Monu", 15.5));
-		empList.add(new Employee(103, "Tonu", 12.5));
-		return empList;
-	}
-
-//	@RequestMapping("/rrrrr")
-//	@GetMapping("/ggggg")
-//	@PostMapping("/aaaa")
-//	@PutMapping("/ppp")
-//	@DeleteMapping("/ddddd")
+//	public Employee getEmpById(int eid) {
+//		return null;
+//	}
+//
+//	public Employee addEmp(Employee employee) {
+//		return null;
+//	}
 
 }
+
+//@RestController
+//public class EmployeeController {
+//
+//	@GetMapping("/getemp")
+//	public Employee getEmployee() {
+//		System.out.println("getEmployee");
+//		return new Employee(101, "Sonu", 10.5);
+//	}
+//
+//	@GetMapping("/getallemp")
+//	public List<Employee> getAllEmployees() {
+//		System.out.println("getAllEmployees");
+//		List<Employee> empList = new ArrayList<>();
+//		empList.add(new Employee(101, "Sonu", 10.5));
+//		empList.add(new Employee(102, "Monu", 15.5));
+//		empList.add(new Employee(103, "Tonu", 12.5));
+//		return empList;
+//	}
+//
+////	@RequestMapping("/rrrrr")
+////	@GetMapping("/ggggg")
+////	@PostMapping("/aaaa")
+////	@PutMapping("/ppp")
+////	@DeleteMapping("/ddddd")
+//
+//}
---
+++ src/main/java/com/cg/spring/boot/demo/service/EmployeeService.java
@@ -0,0 +1,38 @@
+package com.cg.spring.boot.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.cg.spring.boot.demo.model.Employee;
+
+//@Component
+
+@Service
+public class EmployeeService {
+
+	private List<Employee> empList = new ArrayList<>();
+
+	{
+		empList.add(new Employee(101, "Sonu", 10.5));
+		empList.add(new Employee(102, "Monu", 15.5));
+		empList.add(new Employee(103, "Tonu", 12.5));
+	}
+
+	public List<Employee> getAllEmployees() {
+		System.out.println("Service getAllEmployees");
+		return empList;
+	}
+
+//	public Employee getEmployeeById(int eid) {
+//
+//		return new Employee();
+//	}
+//
+//	public Employee addEmployee(Employee employee) {
+//		return employee;
+//	}
+
+}

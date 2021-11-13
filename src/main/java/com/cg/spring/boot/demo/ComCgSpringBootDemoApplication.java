4d73d3fe079049b621de55a55ad0c45a7b097265
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-13 11:18:39 +0530

    Commit

--- src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoApplication.java
+++ src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoApplication.java
@@ -1,16 +1,20 @@
 package com.cg.spring.boot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComCgSpringBootDemoApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ComCgSpringBootDemoApplication.class);
+
 	public static void main(String[] args) {
 
-		System.out.println("Start");
+		LOG.info("Start");
 		SpringApplication.run(ComCgSpringBootDemoApplication.class);
-		System.out.println("End");
+		LOG.info("End");
 
 	}
 }
--- src/main/java/com/cg/spring/boot/demo/Hello.java
+++ src/main/java/com/cg/spring/boot/demo/Hello.java
@@ -1,17 +1,21 @@
 package com.cg.spring.boot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
-	// URL - http://localhost:8082/hello
-	// Endpoint, API - hello
-	@RequestMapping("/hello") // endpoint , API
+	private static final Logger LOG = LoggerFactory.getLogger(Hello.class);
+
+	// http://localhost:8082/hello
+	@RequestMapping("/hello")
 	public String hello() {
-		System.out.println("hello");
-		return "Hello world!";
+//		System.out.println("hello");
+		LOG.info("hello");
+		return "Hello world....!";
 	}
 
 // http://localhost:8082/hi
